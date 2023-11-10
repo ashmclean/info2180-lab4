1c9c37f5adff09a603ec66d419f8619ef8bf6b8b
Ashleigh McLean <ashmclean55@gmail.com>
2023-11-09 22:44:47 -0500

    Exercise 2

--- superheroes.php
+++ superheroes.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type');
+
 $superheroes = [
   [
       "id" => 1,
